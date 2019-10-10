ac3b313d70794927fd596ec37f4ad3794adaaaf6
Denis Zaratan Pasin <denis@pasin.fr>
2019-10-10 20:39:02 +0900

    local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,46 +1,114 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Rule from "./Rule";
 
 const data = [
-  "Every action must be tab-able",
-  "Maximum 3 colours on a page",
-  "All the texts need to be readable / have an excellent contrast",
-  "Fonts must be READABLE (read standard/or have a good reason to be weird).",
-  "Texts need to be understandable (non-technical)",
-  "The elements of the page need to have white space around them",
-  "The number of interactions to achieve a goal need to be minimal",
-  "The user needs to be able to understand what does the website do (be clear)",
-  "Call to actions must use action verbs",
-  "Links need to do something visible / shouldn't be dead links.",
-  "Videos & pictures need to load the fastest as possible",
-  "Text on images/backgrounds/videos needs to be EASILY readable PLEASE.",
-  "Elements need to belong together",
-  "Not too many lines/borders. Blank space is an excellent separator.",
-  "‘#’ links are forbidden. This is a button, not a link. ",
-  "Every action must have a hover and a focus",
-  "Every action must be fireable with “space” or “enter”",
-  "Every image must have an alt tag",
-  "Every action must have a label (especially if icon only)",
-  "Every icon-only button must be well known/obvious (unless a particular design).",
-  "Scale down your app slowly to 500px and check if everything makes sense/looks good ALL THE TIME",
-  "Activate mobile mode and check with the slowest mobile and smallest mobile :D"
+  { id: "1", text: "Every action must be tab-able" },
+  { id: "2", text: "Maximum 3 colours on a page" },
+  {
+    id: "3",
+    text: "All the texts need to be readable / have an excellent contrast"
+  },
+  {
+    id: "4",
+    text:
+      "Fonts must be READABLE (read standard/or have a good reason to be weird)."
+  },
+  { id: "5", text: "Texts need to be understandable (non-technical)" },
+  {
+    id: "6",
+    text: "The elements of the page need to have white space around them"
+  },
+  {
+    id: "7",
+    text: "The number of interactions to achieve a goal need to be minimal"
+  },
+  {
+    id: "8",
+    text:
+      "The user needs to be able to understand what does the website do (be clear)"
+  },
+  { id: "9", text: "Call to actions must use action verbs" },
+  {
+    id: "10",
+    text: "Links need to do something visible / shouldn't be dead links."
+  },
+  { id: "11", text: "Videos & pictures need to load the fastest as possible" },
+  {
+    id: "12",
+    text:
+      "Text on images/backgrounds/videos needs to be EASILY readable PLEASE."
+  },
+  { id: "13", text: "Elements need to belong together" },
+  {
+    id: "14",
+    text: "Not too many lines/borders. Blank space is an excellent separator."
+  },
+  { id: "15", text: "‘#’ links are forbidden. This is a button, not a link. " },
+  { id: "16", text: "Every action must have a hover and a focus" },
+  { id: "17", text: "Every action must be fireable with “space” or “enter”" },
+  { id: "18", text: "Every image must have an alt tag" },
+  {
+    id: "19",
+    text: "Every action must have a label (especially if icon only)"
+  },
+  {
+    id: "20",
+    text:
+      "Every icon-only button must be well known/obvious (unless a particular design)."
+  },
+  {
+    id: "21",
+    text:
+      "Scale down your app slowly to 500px and check if everything makes sense/looks good ALL THE TIME"
+  },
+  {
+    id: "22",
+    text:
+      "Activate mobile mode and check with the slowest mobile and smallest mobile :D"
+  }
 ];
 
-const percent = numberOfRules =>
-  Math.round((numberOfRules / data.length) * 100);
+const percent = rulesChecked =>
+  Math.round((rulesChecked.length / data.length) * 100);
 
 export default function App() {
-  const [numberOfRulesChecked, setNumberOfRulesChecked] = useState(0);
+  const [rulesChecked, setRulesChecked] = useState([]);
+
+  useEffect(() => {
+    const key = window.location.hostname;
+    chrome.storage.local.get([key], result => {
+      const value = result[key] || [];
+      setRulesChecked(value);
+    });
+  }, []);
+
+  const toggleRule = ruleId => {
+    let newRules;
+    if (rulesChecked.includes(ruleId)) {
+      newRules = rulesChecked.filter(id => ruleId !== id);
+    } else {
+      newRules = [ruleId, ...rulesChecked];
+    }
+    setRulesChecked(newRules);
+
+    const rulesToSet = {};
+    rulesToSet[window.location.hostname] = newRules;
+
+    chrome.storage.local.set(rulesToSet, function() {
+      console.log("Value is set to " + rulesToSet);
+    });
+  };
+
   return (
     <>
-      <h1>The rulz of ze UX {percent(numberOfRulesChecked)}%</h1>
+      <h1>The rulz of ze UX {percent(rulesChecked)}%</h1>
       <ul>
         {data.map((rule, i) => (
           <Rule
-            text={rule}
-            key={i}
-            numberOfRulesChecked={numberOfRulesChecked}
-            setNumberOfRulesChecked={setNumberOfRulesChecked}
+            rule={rule}
+            key={rule.id}
+            toggleRule={toggleRule}
+            checked={rulesChecked.includes(rule.id)}
           />
         ))}
       </ul>
--- src/components/Rule.jsx
+++ src/components/Rule.jsx
@@ -1,24 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Rule({ numberOfRulesChecked, setNumberOfRulesChecked, text }) {
-  const actOnClick = e => {
-    const newValue = numberOfRulesChecked + (e.currentTarget.checked ? 1 : -1);
-    setNumberOfRulesChecked(newValue);
+function Rule({
+  numberOfRulesChecked,
+  setNumberOfRulesChecked,
+  rule,
+  checked,
+  toggleRule
+}) {
+  const actOnChange = e => {
+    toggleRule(rule.id);
   };
 
   return (
     <li>
-      <input type="checkbox" onClick={actOnClick} />
-      {text}
+      <input
+        name={rule.id}
+        type="checkbox"
+        checked={checked}
+        onChange={actOnChange}
+      />
+      {rule.text}
     </li>
   );
 }
 
 Rule.propTypes = {
-  numberOfRulesChecked: PropTypes.number.isRequired(),
-  setNumberOfRulesChecked: PropTypes.func.isRequired(),
-  text: PropTypes.string.isRequired()
+  rule: PropTypes.any.isRequired,
+  checked: PropTypes.bool.isRequired,
+  toggleRule: PropTypes.func.isRequired
 };
 
 export default Rule;
